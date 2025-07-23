5971b37304520e7c30097c37a7f035bc88890bb8
bmind-ducanh <anhnd@bmind.company>
2025-07-23 22:27:45 +0700

    builder pattern

--- creational-pattern/builder/builder.ts
+++ creational-pattern/builder/builder.ts
@@ -25,6 +25,12 @@ class StudentConcreteBuilder implements StudentBuilder {
   private firstName: string;
   private lastName: string;
 
+  constructor() {
+    this.id = "";
+    this.firstName = "";
+    this.lastName = "";
+  }
+
   public setId(id: string): StudentBuilder {
     this.id = id;
     return this;
@@ -44,3 +50,22 @@ class StudentConcreteBuilder implements StudentBuilder {
     return new Student(this.id, this.firstName, this.lastName);
   }
 }
+
+const main = () => {
+  const student = new StudentConcreteBuilder()
+    .setId("2305")
+    .setFirstName("Nguyen")
+    .setLastName("Duc Anh")
+    .build();
+
+  console.log(student);
+  /*
+    Output:
+    Student {
+      id: '2305',
+      firstName: 'Nguyen',
+      lastName: 'Duc Anh'
+    }
+  */
+};
+main();
